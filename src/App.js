face04f556a8ee5eba88895b2fb32d0b322565d9
miguelfAndrade <miguelandrade.96.18@gmail.com>
2021-08-04 16:07:22 +0100

    Basic react project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
+
+const herokuApp = 'https://mypersonal-api-cv.herokuapp.com/';
+const localHost = 'http://localhost:3001';
 
 function App() {
+  axios.get(localHost).then(function (response) {
+    let data = response.data.PT[0];
+    let data2 = response.data.EN[0];
+    console.log(data);
+    console.log(data2);
+  }).catch(function (error) {
+    console.log(error);
+});
+
   return (
     <div className="App">
       <header className="App-header">
