f51273bffa091acdd5bb10ea316645ec9dd0e3ea
miguelfAndrade <miguelandrade.96.18@gmail.com>
2023-08-29 00:28:53 +0100

    Trying to add the scroll mechanic

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ let allListElements = document.getElementsByClassName('list-element');
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
-let sectionClicked = "about-me";
+// let sectionClicked = "about-me";
+let c = 0;
 
+function activateSection(sectionId) {
+    resetDisplaySections('none');
+    elem = document.getElementById(sectionId);
+    elemList = document.getElementById(sectionId + '-list');
+    elem.style.display = 'inherit';
+    elemList.classList.add("section-active");
+    elemList.firstElementChild.classList.add("dot-active");
+}
 
 function resetDisplaySections(value) {
-    for(i=0; i<allSections.length; i++) {
-        allSections[i].style.display = value;
-        allListElements[i].style.color = '#AAAAAA';
-        allListElements[i].firstElementChild.style = "background-color: white;";
+    for(i=0; i<allListElements.length; i++) {
+        // allSections[i].style.display = value;
+        allListElements[i].classList.remove("section-active");
+        allListElements[i].firstElementChild.classList.remove("dot-active");
     }
 }
 
 
 window.onload = function() {
-    allSections[0].style.display = 'inherit';
-    allListElements[0].style.color = '#5B5B5B';
-    allListElements[0].firstElementChild.style = "background-color: #AAAAAA;";
+    activateSection('about-me');
     for(i=0; i<allSections.length; i++) {
         document.getElementsByClassName('section')[i].style.height = window.innerHeight + 'px';
     }
@@ -30,35 +37,39 @@ window.onresize = function() {
         resetDisplaySections('inherit');
     }
     else {
-        resetDisplaySections('none');
-        allSections[0].style.display = 'inherit';
-        allListElements[0].style.color = '#5B5B5B';
-        allListElements[0].firstElementChild.style = "background-color: #AAAAAA;";
+        activateSection('about-me');
     }
 };
 
+// window.onscroll = function() {
+//     console.log(allSections[0].clientHeight);
+//     // let topScroll = document.documentElement.scrollTop;
+//     // if(0 <= topScroll < allSections[0].clientHeight)
+//     // {
+//     //     window.scroll({top: 0, behavior: "smooth"});
+//     // }
+//     // else if(allSections[0].clientHeight <= topScroll < allSections[1].clientHeight)
+//     // {
+//     //     window.scroll({top: allSections[0].clientHeight, behavior: "smooth"});
+//     // }
+// }
+
 function cursorClicks(id) {
-    sectionClicked = id;
-    resetDisplaySections('none');
-    elem = document.getElementById(id);
-    elemList = document.getElementById(id + '-list');
-    elem.style.display = 'inherit';
-    elemList.style.color = '#5B5B5B';
-    elemList.firstElementChild.style = "background-color: #AAAAAA;";
+    activateSection(id);
 }
 
-function cursorEnters(id) {
-    if(sectionClicked !== id) {
-        elemList = document.getElementById(id + '-list');
-        elemList.style.color = '#5B5B5B';
-        elemList.firstElementChild.style = "background-color: #AAAAAA;";
-    }
-}
+// function cursorEnters(id) {
+//     if(sectionClicked !== id) {
+//         elemList = document.getElementById(id + '-list');
+//         elemList.style.color = '#5B5B5B';
+//         elemList.firstElementChild.style = "background-color: #AAAAAA;";
+//     }
+// }
 
-function cursorLeaves(id) {
-    if(sectionClicked !== id) {
-        elemList = document.getElementById(id + '-list');
-        elemList.style.color = '#AAAAAA';
-        elemList.firstElementChild.style = "background-color: white;";
-    }
-}
+// function cursorLeaves(id) {
+//     if(sectionClicked !== id) {
+//         elemList = document.getElementById(id + '-list');
+//         elemList.style.color = '#AAAAAA';
+//         elemList.firstElementChild.style = "background-color: white;";
+//     }
+// }
