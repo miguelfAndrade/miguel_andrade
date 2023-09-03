52f89867f7625c28051ba53882d2231e336110fc
miguelfAndrade <miguelandrade.96.18@gmail.com>
2023-09-03 21:33:00 +0100

    Website v1.0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My personal site
+
+You can access it here: https://miguelfandrade.github.io/miguel_andrade/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let allSections = document.getElementsByClassName('section');
 let allListElements = document.getElementsByClassName('list-element');
+let allSectionTitles = document.getElementsByClassName('section-title');;
 
 let aboutMeSection = document.getElementById("about-me");
 let experienceSection = document.getElementById("experience");
@@ -17,32 +18,19 @@ let allSectionsHeights = aboutMeHeight + experienceHeight + skillsHeight + langu
 
 let sectionDivision = 1/allSections.length;
 
-let scrollVar = 0;
+let scrollVar;
 
-function activateSection(sectionId) {
-    resetDisplaySections('none');
-    elem = document.getElementById(sectionId);
-    elemList = document.getElementById(sectionId + '-list');
-    elem.style.display = 'inherit';
-    elem.classList.add("section-animation");
-    elemList.classList.add("section-active");
-    elemList.firstElementChild.classList.add("dot-active");
-}
+let mobileWidth = 1100;
 
-function resetDisplaySections(value) {
-    for(i=0; i<allListElements.length; i++) {
-        allSections[i].style.display = value;
-        allSections[i].classList.remove("section-animation");
-        allListElements[i].classList.remove("section-active");
-        allListElements[i].firstElementChild.classList.remove("dot-active");
-    }
-}
-
+let hideIcons = true;
 
 window.onload = function() {
+    allSectionTitles = document.getElementsByClassName('section-title');
+    // animationSocials();
     document.body.style.setProperty('--body-height', ((allListElements.length * window.innerHeight) + 'px'));
-    if(window.innerWidth <= 900) {
+    if(window.innerWidth <= mobileWidth) {
         resetDisplaySections('inherit');
+        hideSocialsIcons('icons-list');
     }
     else {
         activateSection('about-me');
@@ -50,22 +38,22 @@ window.onload = function() {
 }
 
 window.onresize = function() {
-    if(window.innerWidth <= 900) {
+    // animationSocials();
+    if(window.innerWidth <= mobileWidth) {
         resetDisplaySections('inherit');
+        hideSocialsIcons('icons-list');
     }
     else {
         activateSection('about-me');
+        // hideIcons = false;
     }
 };
 
-window.onscroll = function() {
-    
+window.onscroll = function(event) {
+    event.preventDefault();
     scrollVar = document.documentElement.scrollTop / (document.body.offsetHeight - window.innerHeight);
-    
-    // console.log("Scroll Value: " + scrollVar);
-    // console.log("Section Division Value: " + sectionDivision);
-    
-    if(window.innerWidth <= 900) 
+        
+    if(window.innerWidth <= mobileWidth) 
     {
         resetDisplaySections('inherit');
     }
@@ -74,7 +62,6 @@ window.onscroll = function() {
         if(0 < scrollVar <= sectionDivision)
         {
             activateSection('about-me');
-            console.log("About Me!");
         }
         else if(sectionDivision < scrollVar <= (2*sectionDivision))
         {
@@ -98,9 +85,83 @@ window.onscroll = function() {
         }
     }
 
+    // console.log(allSectionTitles[0].offsetHeight);
+    // for(i=0; i<allSectionTitles.length; i++) {
+    //     if(document.body.offsetHeight >= allSectionTitles[i].offsetHeight) {
+    //         allSectionTitles[i].classList.add('shadow-section-title');
+    //     }
+    //     else {
+    //         allSectionTitles[i].classList.remove('shadow-section-title');
+    //     }
+    // }
 }
 
 function cursorClicks(id) {
     activateSection(id);
 }
+
+function hideSocialsIcons(iconsListId) {
+    // animationSocials();
+    let iconsList = document.getElementById(iconsListId);
+    if(hideIcons) {
+        iconsList.classList.remove("icon-list-show");
+        iconsList.classList.add("icon-list-hide");
+    }
+    else {
+        iconsList.classList.remove("icon-list-hide");
+        // iconsList.classList.remove("animation-icons-hide");
+        iconsList.classList.add("icon-list-show");
+        // iconsList.classList.add("animation-icons-show");
+    }
+    hideIcons = !hideIcons;
+}
+
+
+function activateSection(sectionId) {
+    resetDisplaySections('none');
+    let elem = document.getElementById(sectionId);
+    let elemList = document.getElementById(elem.id + '-list');
+    elem.style.display = 'inherit';
+    elem.classList.add("section-animation");
+    elemList.classList.add("section-active");
+    elemList.firstElementChild.classList.add("dot-active");
+    // if(sectionId == 'about-me') {
+    //     document.documentElement.scrollTop = 0;
+    //     console.log("Entrou!!!");
+    // }
+    // else if(sectionId == 'experience') {
+    //     document.documentElement.scrollTop = (sectionDivision)*(document.body.offsetHeight - window.innerHeight);
+    // }
+    // else if(sectionId == 'studies') {
+    //     document.documentElement.scrollTop = (2*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
+    // }
+    // else if(sectionId == 'skills') {
+    //     document.documentElement.scrollTop = (3*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
+    // }
+    // else if(sectionId == 'languages') {
+    //     document.documentElement.scrollTop = (4*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
+    // }
+    // else if(sectionId == 'projects') {
+    //     document.documentElement.scrollTop = (5*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
+    // }
+
+    // console.log(scrollVar);
+    // console.log(document.documentElement.scrollTop);
+}
+
+function resetDisplaySections(value) {
+    for(i=0; i<allListElements.length; i++) {
+        allSections[i].style.display = value;
+        allSections[i].classList.remove("section-animation");
+        allListElements[i].classList.remove("section-active");
+        allListElements[i].firstElementChild.classList.remove("dot-active");
+    }
+}
+
+// function animationSocials() {
+//     let windowHeight = window.innerHeight;
+//     let socialsHeight = document.getElementById('contacts-icons').clientHeight;
+//     document.body.style.setProperty('--social-animations-from', (windowHeight + socialsHeight) + 'px');
+//     document.body.style.setProperty('--social-animations-to', (windowHeight - socialsHeight) + 'px');
+// }
 
