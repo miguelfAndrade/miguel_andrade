906f05dc283c56c8a9ac7da91580796a6357d931
miguelfAndrade <miguelandrade.96.18@gmail.com>
2024-05-01 23:51:26 +0200

    Refactor of the code, dynamic age in the text and fix of some bugs

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 let allSections = document.getElementsByClassName('section');
 let allListElements = document.getElementsByClassName('list-element');
 
+let birthDate = new Date("1996-01-18");
+let ageText = document.getElementById('age');
+
 let mainTitleHeight = document.getElementById('main-title').clientHeight;
 
 let contactsIcons = document.getElementById('icons-list');
@@ -13,18 +16,20 @@ let linkMoreInfoSkill = document.getElementById("more-info-link-skill");
 let showMoreInfoSkill = false;
 
 let aboutMeSection = document.getElementById("about-me");
+let profissionalProjectsSection = document.getElementById("profissionalProjects");
 let experienceSection = document.getElementById("experience");
 let skillsSection = document.getElementById("skills");
 let languagesSection = document.getElementById("languages");
-let projectsSection = document.getElementById("projects");
+let personalProjectsSection = document.getElementById("personalProjects");
 
 let aboutMeHeight = aboutMeSection.clientHeight;
+let profissionalProjectsHeight = profissionalProjectsSection.clientHeight;
 let experienceHeight = experienceSection.clientHeight;
 let skillsHeight = skillsSection.clientHeight;
 let languagesHeight = languagesSection.clientHeight;
-let projectsHeight = projectsSection.clientHeight;
+let personalProjectsHeight = personalProjectsSection.clientHeight;
 
-let allSectionsHeights = aboutMeHeight + experienceHeight + skillsHeight + languagesHeight + projectsHeight;
+let allSectionsHeights = aboutMeHeight + profissionalProjectsHeight + experienceHeight + skillsHeight + languagesHeight + personalProjectsHeight;
 
 let sectionDivision = 1/allSections.length;
 
@@ -47,6 +52,7 @@ window.onload = function() {
         sectionCurrentlyActive = 'about-me';
     }
     activateSection(sectionCurrentlyActive);
+    ageText.innerHTML = calculateAge();
 }
 
 window.onresize = function() {
@@ -60,7 +66,9 @@ window.onresize = function() {
     }
 };
 
-window.onscroll = function(event) {
+window.onscroll = function(event) {scrollDynamic(event)}
+
+function scrollDynamic(event) {
     event.preventDefault();
     scrollVar = document.documentElement.scrollTop / (document.body.offsetHeight - window.innerHeight);
         
@@ -71,41 +79,22 @@ window.onscroll = function(event) {
     else 
     {
         sectionLastActive = sectionCurrentlyActive;
-        if(0 < scrollVar <= sectionDivision)
-        {
-            sectionCurrentlyActive = 'about-me';
-        }
-        else if(sectionDivision < scrollVar <= (2*sectionDivision))
-        {
-            sectionCurrentlyActive = 'experience';
-        }
-        else if((2*sectionDivision) < scrollVar <= (3*sectionDivision))
-        {
-            sectionCurrentlyActive = 'studies';
-        }
-        else if((3*sectionDivision) < scrollVar <= (4*sectionDivision))
-        {
-            sectionCurrentlyActive = 'skills';
-        }
-        else if((4*sectionDivision) < scrollVar <= (5*sectionDivision))
-        {
-            sectionCurrentlyActive = 'languages';
-        }
-        else if((5*sectionDivision) < scrollVar <= (6*sectionDivision))
-        {
-            sectionCurrentlyActive = 'projects';
+        for (let i = 0; i < allSections.length; i++) {
+            if((i*sectionDivision) < scrollVar <= ((i+1)*sectionDivision)) {
+                sectionCurrentlyActive = allSections.item(i).id;
+                break;
+            }
         }
 
         if(sectionCurrentlyActive !== sectionLastActive) {
             activateSection(sectionCurrentlyActive);
         }
     }
-
 }
 
 contactIconsButton.addEventListener('click', () => {
     showHideIcons = !showHideIcons;
-    console.log(showHideIcons);
+    // console.log(showHideIcons);
     seeSocialsIcons();
 })
 
@@ -156,7 +145,7 @@ function activateSection(sectionId) {
     // else if(sectionId == 'languages') {
     //     document.documentElement.scrollTop = (4*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
     // }
-    // else if(sectionId == 'projects') {
+    // else if(sectionId == 'personalProjects') {
     //     document.documentElement.scrollTop = (5*sectionDivision)*(document.body.offsetHeight - window.innerHeight);
     // }
 
@@ -177,18 +166,37 @@ function resetDisplaySections(value) {
 
 // Function to show pop-up for additional content
 
-function seeMoreInfoSkill() {
-    showMoreInfoSkill = !showMoreInfoSkill
-    if(showMoreInfoSkill) {
-        moreInfoSkill.style.display = 'initial';
-        moreInfoSkill.classList.remove('hide-more-info-animation');
-        moreInfoSkill.classList.add('show-more-info-animation');
-        linkMoreInfoSkill.innerHTML = 'Less information';
+function seeMoreInfo(idLinkMore, idLinkLess, idElem, showHide) {
+    let elem = document.getElementById(idElem);
+    let linkMore = document.getElementById(idLinkMore);
+    let linkLess = document.getElementById(idLinkLess);
+    if(showHide) {
+        elem.style.display = 'initial';
+        elem.classList.remove('hide-more-info-animation');
+        elem.classList.add('show-more-info-animation');
+        // link.innerHTML = 'Less information';
+        linkMore.style.display = 'none';
+        linkLess.style.display = 'initial';
     }
     else {
-        moreInfoSkill.classList.remove('show-more-info-animation');
-        moreInfoSkill.classList.remove('hide-more-info-animation');
-        moreInfoSkill.style.display = 'none';
-        linkMoreInfoSkill.innerHTML = 'More information';
+        elem.classList.remove('show-more-info-animation');
+        elem.classList.remove('hide-more-info-animation');
+        elem.style.display = 'none';
+        // link.style.display = 'More information';
+        linkMore.style.display = 'initial';
+        linkLess.style.display = 'none';
     }
+}
+
+function calculateAge () {
+    let todayDate = new Date();
+    todayDate.toLocaleString("pt-PT");
+
+    let years = (todayDate.getFullYear() - birthDate.getFullYear());
+    if (todayDate.getMonth() < birthDate.getMonth() || 
+        todayDate.getMonth() == birthDate.getMonth() && todayDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
+
+    return years;
 }
